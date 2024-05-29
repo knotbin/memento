7452316a7721e7a758b3baa7fa6592cd7952f922
Roscoe Rubin-Rottenberg <roscoe@knotbin.xyz>
2024-05-29 10:21:40 -0400

    delete debug statements

--- ShareSheet/ShareView.swift
+++ ShareSheet/ShareView.swift
@@ -22,15 +22,12 @@ struct ShareView: View {
                     extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
                     return
                 }
-                print(input)
                 for provider in input {
                     provider.loadItem(forTypeIdentifier: "public.url") { data, _ in
                         if let url = data as? URL {
-                            print(url)
                             let item = Item(timestamp: Date(), link: url.absoluteString, url: url)
                             modelContext.insert(item)
                             extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
-                            print("added")
                         }
                     }
                 }
