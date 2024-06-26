954ac59e88adfcd3dc5263212b9bf3a2b430a027
Roscoe Rubin-Rottenberg <roscoe@knotbin.xyz>
2024-06-25 23:47:16 -0400

    add note list view

---
+++ Memento/Views/NoteListView.swift
@@ -0,0 +1,25 @@
+//
+//  NoteListView.swift
+//  Memento
+//
+//  Created by Roscoe Rubin-Rottenberg on 6/25/24.
+//
+
+import SwiftUI
+import SwiftData
+import WidgetKit
+
+struct NoteListView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query(sort: \Note.timestamp, order: .reverse, animation: .smooth) private var notes: [Note]
+    
+    var body: some View {
+        List(notes) { note in
+            NoteView(note: note)
+        }
+    }
+}
+
+#Preview {
+    NoteListView()
+}
