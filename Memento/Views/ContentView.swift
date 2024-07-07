d284b56edb8e8f3573c7c9abdafe8524ae6c501c
Roscoe Rubin-Rottenberg <roscoe@knotbin.xyz>
2024-07-07 14:13:04 -0400

    ios 18 icons

--- Memento/Views/ContentView.swift
+++ Memento/Views/ContentView.swift
@@ -13,14 +13,19 @@ struct ContentView: View {
     let modelContext = ModelContext(ConfigureModelContainer())
 
     var body: some View {
-        NavigationStack {
-            ListView()
-            .toolbar { Button("Add Item", systemImage: "plus", action: viewModel.addSheet) }
-            .navigationTitle("Items")
-            .sheet(isPresented: $viewModel.sheetShown, content: {
-                AddView(shown: $viewModel.sheetShown)
-            })
-            .modelContext(modelContext)
+        TabView {
+            NavigationStack {
+                ListView()
+                .toolbar { Button("Add Item", systemImage: "plus", action: viewModel.addSheet) }
+                .navigationTitle("Links")
+                .sheet(isPresented: $viewModel.sheetShown, content: {
+                    AddView(shown: $viewModel.sheetShown)
+                })
+                .modelContext(modelContext)
+            }
+            .tabItem({Label("Links", systemImage: "link")})
+            Text("Not links")
+                .tabItem({Label("Items", systemImage: "rectangle")})
         }
     }
 }
