34c9e45855db9e3e848b8fef5e98123a40c11b95
Roscoe Rubin-Rottenberg <roscoe@knotbin.xyz>
2024-07-09 12:24:33 -0400

    menu

--- Memento/Views/DetailView.swift
+++ Memento/Views/DetailView.swift
@@ -15,9 +15,10 @@ struct DetailView: View {
     var item: Item
     @Binding var selectedItem: Item?
     var body: some View {
-        VStack(alignment: .leading) {
-            if horizontalSizeClass == .regular {
-                HStack {
+        NavigationStack {
+            ScrollView {
+                Text("Created \(item.timestamp.formatted())")
+                VStack(alignment: .leading) {
                     GroupBox(label: Label("Link", systemImage: "link")) {
                         HStack {
                             if let data = item.metadata?.siteImage, let image = UIImage(data: data) {
@@ -36,80 +37,38 @@ struct DetailView: View {
                             }
                         }
                     }
-                    VStack {
-                        GroupBox(label: Label("Created", systemImage: "calendar")) {
-                            Text(item.timestamp.formatted())
-                        }
-                        GroupBox(label: Label("Actions", image: "pencil")) {
-                            HStack {
-                                Button(item.viewed ? "Unmark Viewed" : "Mark Viewed", systemImage: item.viewed ? "book.fill" : "book") {
-                                    withAnimation {
-                                        item.viewed.toggle()
-                                        UpdateAll()
-                                    }
-                                }
-                                .buttonStyle(.bordered)
-                                Button("Delete", systemImage: "trash") {
-                                    withAnimation {
-                                        modelContext.delete(item)
-                                        selectedItem = nil
-                                    }
-                                }
-                                .buttonStyle(.bordered)
-                            }
-                        }
-                    }
+                    .padding()
+                    Text(item.note ?? "")
+                        .multilineTextAlignment(.leading)
+                        .tint(.secondary)
+                        .font(.system(size: 20))
+                        .padding(.horizontal)
                 }
-                .padding()
-            } else if horizontalSizeClass == .compact {
-                VStack {
-                    GroupBox(label: Label("Link", systemImage: "link")) {
-                        HStack {
-                            if let data = item.metadata?.siteImage, let image = UIImage(data: data) {
-                                Image(uiImage: image)
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(maxHeight: 100)
-                                    .cornerRadius(10)
-                                    .shadow(radius: 2)
-                            }
-                            VStack(alignment: .leading) {
-                                Text(item.metadata?.title ?? "")
-                                    .font(.title).bold()
-                                    .tint(Color.primary)
-                                Text(item.link ?? "")
-                            }
-                        }
-                    }
-                    GroupBox(label: Label("Created", systemImage: "calendar")) {
-                        Text(item.timestamp.formatted())
-                    }
-                    GroupBox(label: Label("Actions", systemImage: "pencil")) {
-                        HStack {
-                            Button(item.viewed ? "Unmark Viewed" : "Mark Viewed", systemImage: item.viewed ? "book.fill" : "book") {
-                                withAnimation {
-                                    item.viewed.toggle()
-                                    UpdateAll()
-                                }
-                            }
-                            .buttonStyle(.bordered)
-                            Button("Delete", systemImage: "trash") {
-                                withAnimation {
-                                    modelContext.delete(item)
-                                    selectedItem = nil
-                                }
-                            }
-                            .buttonStyle(.bordered)
-                        }
-                    }
-                }
-                .padding()
             }
-            Text(item.note ?? "")
-                .multilineTextAlignment(.leading)
-                .tint(.secondary)
-                .font(.system(size: 20))
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Menu {
+                        Button(item.viewed ? "Unmark Viewed" : "Mark Viewed", systemImage: item.viewed ? "book.fill" : "book") {
+                            withAnimation {
+                                item.viewed.toggle()
+                                UpdateAll()
+                            }
+                        }
+                        .buttonStyle(.bordered)
+                        Button("Delete", systemImage: "trash", role: .destructive) {
+                            withAnimation {
+                                modelContext.delete(item)
+                                UpdateAll()
+                                selectedItem = nil
+                            }
+                        }
+                    } label: {
+                        Label("Actions", systemImage: "ellipsis.circle")
+                    }
+                }
+            }
+            .navigationTitle(item.metadata?.title ?? item.note ?? "Item")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
